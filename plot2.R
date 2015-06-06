2bbb9ec07a4bceafa56ca7d806003ec7d11bd607
GautierROOS <roosgautier@orange.fr>
2015-06-06 17:41:43 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 ## Getting full dataset
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+data_full <- read.csv("C:/Users/LV/Documents/Mes documents/GAUTIER/EMPLOI/MOOC/exdata-data-household_power_consumption/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
@@ -13,6 +13,6 @@ data$Datetime <- as.POSIXct(datetime)
 
 ## Plot 2
 plot(data$Global_active_power~data$Datetime, type="l",
-     ylab="Global Active Power (kilowatts)", xlab="")
+     ylab="Global Active Power (kilowatt)", xlab="")
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
